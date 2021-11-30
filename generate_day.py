cb43a2f9badcf48e8cb8ccc5cee48253bb27de92
weichslgartner <weichslgartner@gmail.com>
2021-11-30 21:54:19 +0100

    refactoring and gitignore

--- generate_day.py
+++ generate_day.py
@@ -12,9 +12,9 @@ class Language(str, Enum):
 
 extension = {Language.Python: "py", Language.Haskell: "hs"}
 
-cookie = open(".cookie", "r").readline().strip()
+cookie = open(".cookie", 'r').readline().strip()
 cookies = {'session': cookie}
-year = 2021
+year = 2020
 
 
 def python_stub(day):
@@ -63,31 +63,33 @@ readInt = read
 def get_input(day: int):
     input_file = Path("inputs") / f"input_{day:02d}.txt"
     input_file.parent.mkdir(parents=True, exist_ok=True)
-    if not input_file.exists():
-        print(f"Fetching day {day}")
-        raw_input = get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies)
-        with input_file.open("w") as f:
-            f.writelines(raw_input.content.decode('utf-8'))
+    if input_file.exists():
+        return
+    print(f"Fetching day {day}")
+    raw_input = get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies)
+    with input_file.open('w') as f:
+        f.writelines(raw_input.content.decode('utf-8'))
 
 
 def generate_stub(day: int, lang: Language):
     input_file = Path("src") / lang.name / f"day_{day:02d}.{extension[lang]}"
     input_file.parent.mkdir(parents=True, exist_ok=True)
-    if not input_file.exists():
-        print(f"Creating stub day {day} for {lang.name}")
-        with input_file.open("w") as f:
-            if lang == Language.Python:
-                stub = python_stub(day)
-            else:
-                stub = haskell_stub(day)
-            f.write(stub)
+    if input_file.exists():
+        return
+    print(f"Creating stub day {day} for {lang.name}")
+    with input_file.open('w') as f:
+        if lang == Language.Python:
+            stub = python_stub(day)
+        else:
+            stub = haskell_stub(day)
+        f.write(stub)
 
 
 def main():
     today = date.today()
     print("Today is:", today)
     for day in range(1, 26):
-        if year == today.year and day > today.day or today.month != 12 :
+        if today.year == year and (today.day < day or today.month != 12):
             break
         get_input(day)
         generate_stub(day, lang=Language.Python)
