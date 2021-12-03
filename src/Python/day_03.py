822c2eec4fc7164e9c2bda56f3a6751e6dedf1db
weichslgartner <weichslgartner@gmail.com>
2021-12-03 11:38:14 +0100

    day03 refactor

--- src/Python/day_03.py
+++ src/Python/day_03.py
@@ -4,25 +4,19 @@ from collections import Counter
 
 def part_1(lines):
     counts = map(Counter, zip(*lines))
-    gamma = int(''.join(map(lambda count: "1" if count['1'] > count['0'] else "0", counts)),2)
-    return gamma * (~gamma & (2**(len(lines[0]))-1))
+    gamma = int(''.join(map(lambda count: "1" if count['1'] > count['0'] else "0", counts)), 2)
+    return gamma * (~gamma & (2 ** (len(lines[0])) - 1))
 
 
 def part_2(lines):
-    oxygen = bit_select(lines, True)
-    co2 = bit_select(lines, False)
-    return int(oxygen, 2) * int(co2, 2)
+    return bit_select(lines, True) * bit_select(lines, False)
 
 
 def bit_select(lines, keep_most_common):
     for i in range(len(lines[0])):
         if len(lines) == 1:
             break
-        counts = []
-        for line in zip(*lines):
-            counts.append(Counter(line))
-        count = counts[i]
-        new_lines = []
+        count = Counter(list(zip(*lines))[i])
         if count['1'] == count['0']:
             keep = '1'
         elif count['1'] > count['0']:
@@ -32,15 +26,12 @@ def bit_select(lines, keep_most_common):
         # invert
         if not keep_most_common:
             keep = '1' if keep == '0' else '0'
-        for line in lines:
-            if line[i] == keep:
-                new_lines.append(line)
-        lines = new_lines
-    return lines[0]
+        lines = [line for line in lines if line[i] == keep]
+    return int(lines[0], 2)
 
 
 def main():
-    file = Path(__file__).parents[2] / "inputs" / "input_03_test.txt"
+    file = Path(__file__).parents[2] / "inputs" / "input_03.txt"
     with file.open('r') as f:
         lines = f.read().splitlines()
     print("Part 1 :", part_1(lines))
