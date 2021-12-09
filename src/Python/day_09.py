98664edb1bec9631b98704b94c097408a7a98172
weichslgartner <weichslgartner@gmail.com>
2021-12-09 13:21:32 +0100

    day09 refactor

--- src/Python/day_09.py
+++ src/Python/day_09.py
@@ -2,64 +2,64 @@ from collections import namedtuple
 from functools import reduce
 from pathlib import Path
 from queue import SimpleQueue
+from typing import List, Set
 
 
 class Point(namedtuple('Point', 'x y')):
     def __repr__(self):
         return f'{self.y} {self.x}'
 
-def parse_input(lines):
-    grid = []
-    for line in lines:
-        grid.append([int(x) for x in line.strip()])
-    return grid
+
+def parse_input(lines: List[str]) -> List[List[int]]:
+    return [[int(x) for x in line.strip()] for line in lines]
 
 
-def get_neighbours(p :Point, ymax, xmax):
-    points = []
-    if p.x > 0:
-        points.append(Point(p.x-1,p.y))
-    if p.y > 0:
-        points.append(Point(p.x,p.y-1))
-    if p.x < xmax-1:
-        points.append(Point(p.x+1,p.y))
-    if p.y < ymax -1:
-        points.append(Point(p.x,p.y+1))
-    return points
+def is_in_grid(p: Point, p_max: Point) -> bool:
+    return (p.x >= 0) and (p.y >= 0) and (p.x < p_max.x) and (p.y < p_max.y)
 
-def from_grid(p: Point, grid):
+
+def get_neighbours(p: Point, p_max: Point) -> List[Point]:
+    points = [Point(p.x - 1, p.y), Point(p.x, p.y - 1), Point(p.x + 1, p.y), Point(p.x, p.y + 1)]
+    return filter(lambda x: is_in_grid(x, p_max), points)
+
+
+def from_grid(p: Point, grid: List[List[int]]) -> int:
     return grid[p.y][p.x]
 
-def part_1(grid):
+
+def part_1(grid: List[List[int]]) -> (int, List[Point]):
     low_points = []
-    for y,line in enumerate(grid):
-        for x,val in enumerate(line):
-            neighbors = get_neighbours(Point(x,y),len(grid),len(line))
-            min_neigbor = min([from_grid(p,grid) for p in neighbors])
-            if from_grid(Point(x,y),grid) < min_neigbor:
-                low_points.append(Point(x,y))
-    return sum(from_grid(p,grid) for p in low_points) + len(low_points), low_points
+    p_max = Point(len(grid[0]), len(grid))
+    for y, line in enumerate(grid):
+        for x, val in enumerate(line):
+            neighbors = get_neighbours(Point(x, y), p_max)
+            if from_grid(Point(x, y), grid) < min([from_grid(p, grid) for p in neighbors]):
+                low_points.append(Point(x, y))
+    return sum(from_grid(p, grid) for p in low_points) + len(low_points), low_points
 
 
-def part_2(grid, low_points):
+def part_2(grid: List[List[int]], low_points: List[Point]) -> int:
     visited = set()
     basin_size = []
     for lp in low_points:
-        basin_set = set()
-        basin_set.add(lp)
-        visited.add(lp)
-        queue = SimpleQueue()
-        queue.put(lp)
-        while not queue.empty():
-            to_check = set(get_neighbours(queue.get(), len(grid), len(grid[0]) )) - basin_set
-            for p in to_check:
-                if from_grid(p,grid ) != 9 and p not in visited:
-                    queue.put(p)
-                    visited.add(p)
-                    basin_set.add(p)
-        basin_size.append(len(basin_set))
-    basin_size.sort(reverse=True)
-    return reduce(lambda acc,x: acc*x ,basin_size[0:3] ,1)
+        (lambda x: basin_size.append(len(x)) or visited.update(x))(find_basin(lp, grid, visited))
+    return reduce(lambda acc, x: acc * x, sorted(basin_size, reverse=True)[0:3], 1)
+
+
+def find_basin(lp: Point, grid: List[List[int]], visited: Set[Point]) -> Set[Point]:
+    basin_set = set()
+    basin_set.add(lp)
+    queue = SimpleQueue()
+    queue.put(lp)
+    p_max = Point(len(grid[0]), len(grid))
+    while not queue.empty():
+        to_check = set(get_neighbours(queue.get(), p_max)) - basin_set
+        for p in to_check:
+            if from_grid(p, grid) != 9 and (p not in visited or p not in basin_set):
+                queue.put(p)
+                basin_set.add(p)
+    return basin_set
+
 
 def main():
     file = Path(__file__).parents[2] / "inputs" / "input_09.txt"
@@ -67,8 +67,8 @@ def main():
         lines = f.read().splitlines()
     grid = parse_input(lines)
     result, low_points = part_1(grid)
-    print("Part 1:", result)
-    print("Part 2:", part_2(grid,low_points))
+    print("Part 1:", result)  # 491
+    print("Part 2:", part_2(grid, low_points))  # 1075536
 
 
 if __name__ == '__main__':
