d96fab959ab7782d8a28423eb728f7d25868bf08
weichslgartner <weichslgartner@gmail.com>
2021-12-10 12:39:10 +0100

    day 10 refactor

--- src/Python/day_10.py
+++ src/Python/day_10.py
@@ -1,55 +1,45 @@
 from collections import defaultdict
 from pathlib import Path
-
-costs_corrupt = {')': 3,
-                 ']': 57,
-                 '}': 1197,
-                 '>': 25137}
+from typing import List, Dict
 
 
-def parse_input(lines):
-    return lines
-
-
-def part_1(lines):
+def part_1(lines: List[str]) -> int:
     return sum((lambda x: x[0])(eval_line(line)) for line in lines)
 
 
-def eval_line(line):
-    brackets = {')': '(', ']': '[', '>': '<', '}': '{'}  # {'(': ')', '[': ']', '<': '>', '{': '}'}
+def eval_line(line: str) -> (int, Dict[str, List[int]]):
+    costs_corrupt = {')': 3,
+                     ']': 57,
+                     '}': 1197,
+                     '>': 25137}
+    closted2open = {')': '(', ']': '[', '>': '<', '}': '{'}
     open_chunk = defaultdict(list)
     for pos, c in enumerate(line):
-        if c in brackets.values():
+        if c in closted2open.values():
             open_chunk[c].append(pos)
         else:
-            if len(open_chunk[brackets[c]]) == 0:
-                # print(f"illegal {c} in line {line}")
+            if len(open_chunk[closted2open[c]]) == 0:
                 return costs_corrupt[c], None
             else:
-                open_pos = open_chunk[brackets[c]].pop()
+                open_pos = open_chunk[closted2open[c]].pop()
                 for o in open_chunk.values():
                     if len(o) > 0 and open_pos < o[-1]:
-                        # print(f"illegal {c} in line {line}")
                         return costs_corrupt[c], None
     return 0, open_chunk
 
 
-def part_2(lines):
-    costs = []
-    for line in lines:
-        score, open_chunk = eval_line(line)
-        if score > 0:
-            continue
-        costs.append(eval_autocomplete_costs(open_chunk))
+def part_2(lines: List[str]) -> int:
+    costs = [eval_autocomplete_costs(open_chunk) for _, open_chunk in
+             filter(lambda x: x[0] == 0, map(eval_line, lines))]
     return sorted(costs)[len(costs) // 2]
 
 
-def eval_autocomplete_costs(open_chunk):
+def eval_autocomplete_costs(open_chunk: Dict[str, List[int]]) -> int:
     costs_dict = {')': 1,
                   ']': 2,
                   '}': 3,
                   '>': 4}
-    brackets = {'(': ')', '[': ']', '<': '>', '{': '}'}
+    open2close = {'(': ')', '[': ']', '<': '>', '{': '}'}
     line_costs = 0
     while len(open_chunk) > 0:  # ])}>
         max_pos = -1
@@ -60,7 +50,7 @@ def eval_autocomplete_costs(open_chunk):
                 max_c = k
         if len(open_chunk[max_c]) > 0:
             open_chunk[max_c].pop()
-            line_costs = line_costs * 5 + costs_dict[brackets[max_c]]
+            line_costs = line_costs * 5 + costs_dict[open2close[max_c]]
         else:
             return line_costs
     return line_costs
@@ -70,7 +60,6 @@ def main():
     file = Path(__file__).parents[2] / "inputs" / "input_10.txt"
     with file.open('r') as f:
         lines = f.read().splitlines()
-    lines = parse_input(lines)
     print("Part 1:", part_1(lines))
     print("Part 2:", part_2(lines))
 
