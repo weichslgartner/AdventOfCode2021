c88ffc333d930e1a218612bd0dd7fe019f239772
weichslgartner <weichslgartner@gmail.com>
2021-12-14 08:04:53 +0100

    day 14 refactor

--- src/Python/day_14.py
+++ src/Python/day_14.py
@@ -1,70 +1,52 @@
-from collections import Counter, defaultdict
-from copy import copy, deepcopy
+from collections import defaultdict
 from pathlib import Path
+from typing import Dict, List
 
 
-def parse_input(lines):
-    input = lines[0]
-    insertion_dict = {}
-    for line in lines[2:]:
-        t = line.split('->')
-        insertion_dict[t[0].strip()] = t[1].strip()
-
-    return input,insertion_dict
+def parse_input(lines: List[str]) -> (str, Dict[str, str]):
+    return lines[0], {k.strip(): v.strip() for k, v in map(lambda l: l.split('->'), lines[2:])}
 
 
-def part_1(input,insertion_dict,rounds=10):
-    for r in range(rounds):
-        new_string = input[0]
-        for i in range(0,len(input)-1,1):
-            token = input[i:i+2]
-            new_string += insertion_dict[token] + token[1]
-        input = new_string
-        counter = Counter(input)
-    counter = counter.most_common(len(counter))
-    return counter[0][1]-counter[-1][1]
+def part_1(template: str, insertion_dict: Dict[str, str], rounds: int) -> int:
+    return solve(template, insertion_dict, rounds)
 
 
-def part_2(input,insertion_dict,rounds=40):
-    result_dict = {}
-    for k in insertion_dict.keys():
-        result_dict[k] = 0
-    for i in range(0, len(input) - 1, 1):
-        result_dict[input[i:i + 2]]+=1
-    for r in range(rounds):
-        for k,v in list(result_dict.items()):
-            if v > 0:
-                result_dict[k] -=v
-                result_dict[k[0]+insertion_dict[k]] += v
-                result_dict[insertion_dict[k] + k[1]] += v
-
-    counter = get_letter_count(input, result_dict)
-    counter = counter.most_common(len(counter))
-    return counter[0][1] - counter[-1][1]
+def part_2(template: str, insertion_dict: Dict[str, str], rounds: int) -> int:
+    return solve(template, insertion_dict, rounds)
 
 
-def get_letter_count(input, result_dict):
+def solve(template: str, insertion_dict: Dict[str, str], rounds: int) -> int:
+    result_dict = {k: 0 for k in insertion_dict.keys()}
+    for i in range(len(template) - 1):
+        result_dict[template[i:i + 2]] += 1
+    for _ in range(rounds):
+        for k, v in list(filter(lambda x: x[1] > 0, result_dict.items())):
+            result_dict[k] -= v
+            result_dict[k[0] + insertion_dict[k]] += v
+            result_dict[insertion_dict[k] + k[1]] += v
+    frequency = get_letter_count(template, result_dict)
+    return frequency[-1] - frequency[0]
+
+
+def get_letter_count(template: str, result_dict: Dict[str, int]) -> List[int]:
     letter_count = defaultdict(int)
     for k, v in result_dict.items():
         letter_count[k[0]] += v
         letter_count[k[1]] += v
-    letter_count[input[0]] += 1
-    letter_count[input[-1]] += 1
-    for k,v, in letter_count.items():
-        letter_count[k] = v //2
-    counter = Counter(letter_count)
-    return counter
+    letter_count[template[0]] += 1
+    letter_count[template[-1]] += 1
+    letter_count.update((k, v // 2) for k, v in letter_count.items())
+    return sorted(letter_count.values())
 
 
 def main():
     file = Path(__file__).parents[2] / "inputs" / "input_14.txt"
     with file.open('r') as f:
         lines = f.read().splitlines()
-    input,insertion_dict = parse_input(lines)
-    print("Part 1:", part_1(input,insertion_dict))
-    print("Part 2:", part_2(input,insertion_dict))
+    template, insertion_dict = parse_input(lines)
+    print("Part 1:", part_1(template, insertion_dict, 10))
+    print("Part 2:", part_2(template, insertion_dict, 40))
 
-#2304722022017 too low
 
 if __name__ == '__main__':
     main()
