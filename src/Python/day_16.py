1cdfea4a0229919d134e9e0574606686a50bde81
weichslgartner <weichslgartner@gmail.com>
2021-12-16 23:47:17 +0100

    safety day 16 commit

--- src/Python/day_16.py
+++ src/Python/day_16.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from functools import reduce
 
 from aoc import get_lines
@@ -61,10 +62,8 @@ def consume_raw(bitstream, n_bits):
 def part_1(bitstring):
     parse = Field.Version
     versions = []
-    laterals = []
     lateral_raw = ""
     depth = 0
-    sub_packet_type = []
     n_sub_packets = {}
     length_subpackets = []
     operation_stack = []
@@ -86,7 +85,6 @@ def part_1(bitstring):
                 print(Operations(type))
                 operation_stack.append(Operations(type))
                 expressions.append(Operations.OPEN)
-                depth += 1
                 expressions.append(Operations(type))
                 parse_next = Field.Length_Type_ID
         elif parse == Field.Lateral:
@@ -94,31 +92,14 @@ def part_1(bitstring):
             bitstring, lval = consume_raw(bitstring, 4)
             lateral_raw += lval
             consumed_bits = bit_lengths[Field.Lateral]
-            if len(length_subpackets) > 0:
-                length_subpackets[-1] -= consumed_bits
             if not_last == 0:
                 lateral = int(lateral_raw, 2)
-                # print(lateral)
+                if lateral == 2053:
+                    print(lateral)
                 operation_stack.append(lateral)
                 expressions.append(lateral)
-                if len(length_subpackets) > 0 and length_subpackets[-1] == 0:
-                    length_subpackets.pop()
-                    expressions.append(Operations.CLOSE)
-                    depth -= -1
-
                 lateral_raw = ""
                 parse_next = Field.Version
-                while depth in n_sub_packets:
-                    n_sub_packets[depth] -= 1
-                    if n_sub_packets[depth] == 0:
-                        del n_sub_packets[depth]
-                        expressions.append(Operations.CLOSE)
-                        depth -= -1
-                    else:
-                        break
-
-              #  if len(n_sub_packets) > 0 or len(length_subpackets) > 0:
-              #      parse_next = Field.Version
         elif parse == Field.Padding:
             consumed_bits = len(bitstring)
             bitstring, tmp = consume_raw(bitstring, consumed_bits)
@@ -135,41 +116,59 @@ def part_1(bitstring):
             # print("subpackets", val)
             # if operation_stack[-1] == Operations.GT:
             #     print("asd")
+            depth += 1
             n_sub_packets[depth] = val
+
             parse_next = Field.Version
         elif parse == Field.Total_Length_Sub_Packet:
             consumed_bits = bit_lengths[Field.Total_Length_Sub_Packet]
             bitstring, length_subpacket = consume_int(bitstring, consumed_bits)
             print("length packets", length_subpacket)
-            if operation_stack[-1] == Operations.GT:
+            if length_subpacket == 27:
                 print("asd")
             length_subpackets.append(length_subpacket)
             parse_next = Field.Version
 
-        if len(length_subpackets) > 0 and parse != Field.Lateral:
+        if len(length_subpackets) > 0:
             if not parse == Field.Total_Length_Sub_Packet:
                 length_subpackets[-1] -= consumed_bits
                 if length_subpackets[-1] <= 0:
                     length_subpackets.pop()
+                    expressions.append(Operations.CLOSE)
             if len(length_subpackets) > 1:
                 for i in range(len(length_subpackets) - 1):
                     length_subpackets[i] -= consumed_bits
 
+        if parse == Field.Lateral and parse_next == Field.Version:
+            while depth in n_sub_packets:
+                n_sub_packets[depth] -= 1
+                if n_sub_packets[depth] == 0:
+                    del n_sub_packets[depth]
+                    expressions.append(Operations.CLOSE)
+                    depth -= 1
+                else:
+                    break
+            if len(length_subpackets) > 0 and length_subpackets[-1] == 0:
+                length_subpackets.pop()
+                expressions.append(Operations.CLOSE)
+                #depth -= -1
         parse = parse_next
-        print(n_sub_packets)
+       # print(n_sub_packets)
+       # print(length_subpackets)
     # print(versions)
-    print(expressions)
-    print(operation_stack)
+   # print(expressions)
+    #print(operation_stack)
     return sum(versions), expressions
 
 
 def part_2(operation_stack):
-    result = []
+    results = defaultdict(list)
     operands = []
     if operation_stack[-1] == Operations.Sum:
         operation_stack.pop()
     if operation_stack[-1] == Operations.OPEN:
         operation_stack.pop()
+    operator = []
     n_close = 0
     while len(operation_stack) > 0:
         val = operation_stack.pop()
@@ -178,43 +177,46 @@ def part_2(operation_stack):
             continue
         if val == Operations.OPEN:
             n_close -= 1
+            if operator[-1] in [Operations.LT, Operations.GT, Operations.EQ]:
+                while len(operands) <2:
+                    operands.append(results[n_close+1].pop())
+            else:
+                while len(results[n_close+1]) > 0:
+                    operands.append(results[n_close+1].pop())
+
+            results[n_close].append( calculate_expression(operands, operator.pop()))
+            operands = []
             continue
-        if not isinstance(val, Operations) and n_close > 0:
+        if not isinstance(val, Operations) :
             operands.append(val)
         else:
-            print(operands)
-            if len(operands) == 0:
-                i = 0
-                while len(result) > 0 and isinstance(result[-1], int):
-                    operands.append(result.pop())
-                    i += 1
-                    if val in [Operations.EQ, Operations.GT, Operations.LT] and len(operands) == 2:
-                        break
+            operator.append(val)
 
-            if val == Operations.Sum:
-                result.append(sum(operands))
 
-            elif val == Operations.Product:
-                result.append(reduce(lambda acc, x: acc * x, operands, 1))
-            elif val == Operations.Maximum:
-                result.append(max(operands))
-            elif val == Operations.Minimum:
-                result.append(min(operands))
-            elif val == Operations.GT:
-                print(len(operands))
-                assert len(operands) == 2
-                result.append(int(operands[1] > operands[0]))
-            elif val == Operations.LT:
-                assert len(operands) == 2
 
-                result.append(int(operands[1] < operands[0]))
-            elif val == Operations.EQ:
-                assert len(operands) == 2
+    return results[0][0]
 
-                result.append(int(operands[0] == operands[1]))
-            operands = []
 
-    return result[0]
+def calculate_expression(operands, val):
+    if val == Operations.Sum:
+        return sum(operands)
+    elif val == Operations.Product:
+        return reduce(lambda acc, x: acc * x, operands, 1)
+    elif val == Operations.Maximum:
+        return max(operands)
+    elif val == Operations.Minimum:
+        return min(operands)
+    elif val == Operations.GT:
+        print(operands)
+
+        assert len(operands) == 2
+        return int(operands[1] > operands[0])
+    elif val == Operations.LT:
+        assert len(operands) == 2
+        return int(operands[1] < operands[0])
+    elif val == Operations.EQ:
+        assert len(operands) == 2
+        return int(operands[0] == operands[1])
 
 
 def hex_to_bits(hex_stream):
@@ -222,28 +224,28 @@ def hex_to_bits(hex_stream):
 
 
 def test():
-    # _, stack = part_1(hex_to_bits("C200B40A82"))
-    # assert part_2(stack) == 3
-    # _, stack = part_1(hex_to_bits("04005AC33890"))
-    # assert part_2(stack) == 54
-    # _, stack = part_1(hex_to_bits("880086C3E88112"))
-    # assert part_2(stack) == 7
-    # _, stack = part_1(hex_to_bits("CE00C43D881120"))
-    # assert part_2(stack) == 9
-    # _, stack = part_1(hex_to_bits("D8005AC2A8F0"))
-    # assert part_2(stack) == 1
-    # _, stack = part_1(hex_to_bits("F600BC2D8F"))
-    # assert part_2(stack) == 0
-    # _, stack = part_1(hex_to_bits("9C005AC2F8F0"))
-    # assert part_2(stack) == 0
-    # _, stack = part_1(hex_to_bits("9C0141080250320F1802104A08"))
-    # assert part_2(stack) == 1
-    # result, _ = part_1(hex_to_bits("8A004A801A8002F478"))
-    # assert result == 16
-    # result, _ = part_1(hex_to_bits("620080001611562C8802118E34"))
-    # assert result == 12
-    # result, _ = part_1(hex_to_bits("C0015000016115A2E0802F182340"))
-    # assert result == 23
+    _, stack = part_1(hex_to_bits("C200B40A82"))
+    assert part_2(stack) == 3
+    _, stack = part_1(hex_to_bits("04005AC33890"))
+    assert part_2(stack) == 54
+    _, stack = part_1(hex_to_bits("880086C3E88112"))
+    assert part_2(stack) == 7
+    _, stack = part_1(hex_to_bits("CE00C43D881120"))
+    assert part_2(stack) == 9
+    _, stack = part_1(hex_to_bits("D8005AC2A8F0"))
+    assert part_2(stack) == 1
+    _, stack = part_1(hex_to_bits("F600BC2D8F"))
+    assert part_2(stack) == 0
+    _, stack = part_1(hex_to_bits("9C005AC2F8F0"))
+    assert part_2(stack) == 0
+    _, stack = part_1(hex_to_bits("9C0141080250320F1802104A08"))
+    assert part_2(stack) == 1
+    result, _ = part_1(hex_to_bits("8A004A801A8002F478"))
+    assert result == 16
+    result, _ = part_1(hex_to_bits("620080001611562C8802118E34"))
+    assert result == 12
+    result, _ = part_1(hex_to_bits("C0015000016115A2E0802F182340"))
+    assert result == 23
     result, _ = part_1(hex_to_bits("A0016C880162017C3686B18A3D4780"))
     assert result == 31
 
@@ -256,6 +258,7 @@ def main():
     print("Part 1:", res1)
     ostack.pop()
     ostack.pop()
+    print(ostack)
     print(ostack.count(Operations.OPEN))
     print(ostack.count(Operations.CLOSE))
 
