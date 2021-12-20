68adddd6001f66f793f266b47d9d831a6e29a06d
weichslgartner <weichslgartner@gmail.com>
2021-12-20 19:59:39 +0100

    day19 part 1

--- src/Python/day_19.py
+++ src/Python/day_19.py
@@ -4,7 +4,9 @@ from math import sqrt
 
 from aoc import get_lines, Point3
 
-rotations = [([2, 0, 1], [-1, -1, 1]), ([0, 1, 2], [1, -1, -1]), ([2, 1, 0], [-1, -1, -1]), ([2, 1, 0], [1, -1, 1]),
+rotations = [([2, 0, 1], [-1, -1, 1]),
+             ([0, 1, 2], [1, -1, -1]),
+             ([2, 1, 0], [-1, -1, -1]), ([2, 1, 0], [1, -1, 1]),
              ([0, 2, 1], [-1, -1, -1]), ([1, 2, 0], [1, -1, -1]), ([1, 0, 2], [-1, -1, -1]), ([1, 2, 0], [1, 1, 1]),
              ([0, 2, 1], [-1, 1, 1]), ([0, 1, 2], [-1, 1, -1]), ([0, 2, 1], [1, -1, 1]), ([2, 0, 1], [-1, 1, -1]),
              ([1, 0, 2], [1, 1, -1]), ([2, 1, 0], [1, 1, -1]), ([2, 0, 1], [1, 1, 1]), ([2, 1, 0], [-1, 1, 1]),
@@ -81,84 +83,54 @@ def part_1(scanners):
     mapping_dict, perms, signs = get_relative_centers(intersects, scanners)
     print(mapping_dict)
 
-    print(perms,signs)
+    print(perms, signs)
     to_transform = [[160, -1134, -23]]
     trans_perm = [0, 1, 2]
     trans_sign = [-1, 1, -1]
     target_center = [68, -1246, -43]
-    transform(list(zip(*to_transform)),target_center, trans_perm, trans_sign)
+    transform(list(zip(*to_transform)), target_center, trans_perm, trans_sign)
     transformed_scanners = set()
-    beacons = set(to_point(p) for p in scanners[0]  )
+    beacons = set(to_point(p) for p in scanners[0])
     queue = deque()
     used_mappings = set()
     transformed_scanners.add(0)
     for key in mapping_dict.keys():
         if key[0] == 0:
-            queue.append( key)
-    while len(queue) >0:
-        el = queue.popleft()
-        print(el)
-        p_tanspose = list(zip(*scanners[el[1]]))
-        use_mapping = el
-        new_points = set()
-        while True:
-            p_tanspose = transform(p_tanspose,*mapping_dict[use_mapping])
-            new_points = set(to_point(p) for p in zip(*p_tanspose))
-            if use_mapping[0] == 0:
-                break
-            for mapping in used_mappings:
-                if mapping[1] == use_mapping[0]:
-                    use_mapping = mapping
+            queue.append(key)
+    while len(transformed_scanners) < len(scanners):
+        while len(queue) > 0:
+            el = queue.popleft()
+
+            print(el)
+            p_tanspose = list(zip(*scanners[el[1]]))
+            use_mapping = el
+            while True:
+                p_tanspose = transform(p_tanspose, *mapping_dict[use_mapping])
+                new_points = set(to_point(p) for p in zip(*p_tanspose))
+                if use_mapping[0] == 0:
                     break
+                for mapping in used_mappings:
+                    if mapping[1] == use_mapping[0]:
+                        use_mapping = mapping
+                        break
 
-
-        transformed_scanners.add(el[1])
-        beacons.update(new_points)
-        used_mappings.add(el)
+            transformed_scanners.add(el[1])
+            beacons.update(new_points)
+            used_mappings.add(el)
+            for k in mapping_dict.keys():
+                if k[0] == el[1] and k[1] not in transformed_scanners and k not in used_mappings:
+                    queue.append(k)
         for k in mapping_dict.keys():
-            if k[0] == el[1] and k[1] not in transformed_scanners:
+            if k[0] in transformed_scanners and k[1] not in transformed_scanners:
                 queue.append(k)
-
-
-        print(len(beacons))
     return len(beacons)
-    #  print(list(zip(*rotated)))
-
-  #  a_transpose = list(zip(*points_a))
-   # b_transpose = list(zip(*[160, -1134, -23]))
-
-    #list(zip(*scanners[1]))
-    # for _ in range(3):
-    #     b_transpose = rotate_z(*b_transpose)
-    #     for _ in range(3):
-    #         b_transpose = rotate_y(*b_transpose)
-    #         for _ in range(8):
-    #             b_transpose = rotate_x(*b_transpose)
-    #
-    #             offset = []
-    #             for p in zip(b_transpose, a_transpose):
-    #                 points = set([x[1] - x[0] for x in zip(p[0], p[1])])
-    #                 if len(points) == 1:
-    #                     offset.append(points.pop())
-    #             if len(offset) == 3:
-    #                 print(offset)
-
-    # print(len(transforms))
-
-    # print(b_transpose)
-    # b_transpose = flip_x(*b_transpose)
-    # print(b_transpose)
-    # b_transpose = flip_z(*b_transpose)
-
-    # found = found and len(set([x[1] - x[0] for x in zip(p[0], p[1])])) == 1
-
-    # print(p2dist1)
-    # print(p2dist2)
 
 
 def get_relative_centers(intersects, scanners):
     mapping_dict = {}
     for i in intersects:
+        if i[0] == 16 and i[1] == 4:
+            print("debug")
         p2dist1 = defaultdict(set)
         dist2p1 = defaultdict(list)
         for p in combinations(scanners[i[0]], 2):
@@ -179,7 +151,8 @@ def get_relative_centers(intersects, scanners):
 
         for p in product(p2dist1.keys(), p2dist2.keys()):
             intersect = p2dist1[p[0]].intersection(p2dist2[p[1]])
-            if len(intersect) > 1:
+            if len(intersect) == 11:
+                # print(len(intersect))
                 points_a.append(point_to_list(p[0]))
                 points_b.append(point_to_list(p[1]))
         a_transpose = list(zip(*points_a))
@@ -192,7 +165,8 @@ def get_relative_centers(intersects, scanners):
             offset = []
             for p in zip(rotated, a_transpose):
                 points = set([x[1] - x[0] for x in zip(p[0], p[1])])
-
+                if i[0] == 16 and i[1] == 4:
+                    print(points)
                 if len(points) == 1:
                     offset.append(points.pop())
                 if len(offset) == 3:
@@ -202,7 +176,7 @@ def get_relative_centers(intersects, scanners):
     return mapping_dict, perms, signs
 
 
-def transform(to_transform, target_center,trans_perm, trans_sign):
+def transform(to_transform, target_center, trans_perm, trans_sign):
     transformed = []
     rotated = rotate(to_transform, trans_perm, trans_sign)
     for i, p in enumerate(rotated):
@@ -212,7 +186,7 @@ def transform(to_transform, target_center,trans_perm, trans_sign):
 
 def get_intersections(scanners):
     intersections = []
-    for i in permutations(range(len(scanners)), 2):
+    for i in permutations(range(len(scanners)), 2):  # permutations
         a = set(euclid_distance(*p) for p in combinations(scanners[i[0]], 2))
         b = set(euclid_distance(*p) for p in combinations(scanners[i[1]], 2))
         if len(a.intersection(b)) == 66:
@@ -228,7 +202,7 @@ def main():
     lines = get_lines("input_19.txt")
     scanners = parse_input(lines)
     print(scanners)
-    print("Part 1:", part_1(scanners)) #too low
+    print("Part 1:", part_1(scanners))  # too low
     print("Part 2:", part_2(scanners))
 
 
