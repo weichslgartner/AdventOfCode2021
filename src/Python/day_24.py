94806c04391c9c90222266547eac668270d72848
weichslgartner <weichslgartner@gmail.com>
2021-12-26 22:57:27 +0100

    initial before recursive

--- src/Python/day_24.py
+++ src/Python/day_24.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 from enum import Enum
 
+import z3
+
 from aoc import get_lines
 from operator import add, mul, ifloordiv, mod, eq, iadd, imul, imod
-from z3 import Ints, Optimize, Int, Sum, If, set_param, ArithRef, is_idiv, set_option, set_param
+from z3 import Ints, Optimize, Int, Sum, If, set_param, ArithRef, is_idiv, set_option, set_param, Or
 
 set_param('parallel.enable', True)
 
@@ -56,77 +58,111 @@ def brute_force(instructions):
             return i
 
 
+
+
+
 def part_1(blocks):
     set_option(max_args=10000000, max_lines=1000000, max_depth=10000000, max_visited=1000000)
+    result = ""
+    for i in sorted(blocks.keys(), reverse=True):
 
+        print("position", i)
+        if i == 14:
+            values, w = get_bounds(blocks, i, [])
+
+        else:
+            values, w = get_bounds(blocks, i, values)
+
+        print(values,w)
+        result += str(w)
+    print(result)
+    return result[::-1]
+
+
+def get_bounds(blocks, i, z_values ):
     opt = Optimize()
-    optimize_vars = []
+    results = []
+    optimize_vars = {}
     z_in = Int(f"z_in")
-    #optimize_vars.append(opt.minimize(z_in))
-    z_last = z_in
-    for block_n, block in blocks.items():
-        if block_n != 14:
-            continue
-        block_variables_index = defaultdict(int)
-        z3_variables = {}
-        for ins in block:
-            if ins[0] == Instr.inp:
-                w = Int(f"w_{block_n}")
-                optimize_vars.append(opt.maximize(w))
-                opt.add(w > 0)
-                opt.add(w < 10)
-                z3_variables[f"w_{block_n}"] = w
-            else:
-                var_left = Int(f"{ins[1]}_{block_n}_{block_variables_index[ins[1]]}")
-
-                z3_variables[f"{ins[1]}_{block_n}_{block_variables_index[ins[1]]}"] = var_left
-                block_variables_index[ins[1]] += 1
-                # first introduction of variable x and y
-                if ins[0] == Instr.mul and ins[2] == 0:
-                    opt.add(var_left == 0)
-                else:
-                    if ins[1] == 'z' and block_variables_index[ins[1]] - 2 < 0:
-                        var_right = z_last
-                    else:
-                        var_right = z3_variables[f"{ins[1]}_{block_n}_{block_variables_index[ins[1]] - 2}"]
-                    if isinstance(ins[2], int):
-                        if ins[0] == Instr.div:
-                            opt.add(var_left == var_right / ins[2])
-                        elif ins[0] == Instr.mod:
-                            opt.add(var_left == var_right % ins[2])
-                        elif ins[0] == Instr.add:
-                            opt.add(var_left == var_right + ins[2])
-                        elif ins[0] == Instr.mul:
-                            opt.add(var_left == var_right * ins[2])
-                        elif ins[0] == Instr.eql:
-                            opt.add(var_left == zeql(var_right, ins[2]))
-                        else:
-                            print("error")
-                    elif ins[2] == 'z':
-
-                        opt.add(var_left == z_last)
-                    else:
-                        if ins[2] == 'w':
-                            var_right2 = z3_variables[f"w_{block_n}"]
-                        else:
-                            var_right2 = z3_variables[f"{ins[2]}_{block_n}_{block_variables_index[ins[2]] - 1}"]
-                        if ins[0] == Instr.add:
-                            opt.add(var_left == var_right + var_right2)
-                        elif ins[0] == Instr.mul:
-                            opt.add(var_left == var_right * var_right2)
-                        elif ins[0] == Instr.eql:
-                            opt.add(var_left == zeql(var_right, var_right2))
-                        else:
-                            print("error")
-                if ins[1] == 'z':
-                    z_last = var_left
-    opt.add(z_last == 0)
+    if i == 1:
+        opt.add(z_in == 0)
+    else:
+        opt.add(z_in > 0)
+        opt.add(z_in <  10000)
+    z_last = gen_block_constraints(blocks[i], i, opt, optimize_vars, z_in)
+    if i == 14:
+        opt.add(z_last == 0)
+    if i != 1 and len(z_values) > 0:
+        clauses =[]
+        for val in z_values:
+            clauses.append((z_last ==  val))
+        opt.add(Or(*clauses))
     print(opt.assertions())
-    print(opt.check())
-    print(opt.model())
-    print(z_in)
-    for opti in optimize_vars:
-        print(opti.lower(),opti.upper())
+    #print(opt.check())
+    u = 0
+    while opt.check() == z3.sat and u <35:
+        results.append(opt.model()[z_in])
+        #print(opt.model())
+        opt.add( z_in != opt.model()[z_in])
+        u+=1
+    print(results)
+    return results, optimize_vars[f"w_{i}"].upper()
+
+def gen_block_constraints(block, block_n, opt, optimize_vars, z_in):
+    block_variables_index = defaultdict(int)
+    z3_variables = {}
+    for ins in block:
+        if ins[0] == Instr.inp:
+            w = Int(f"w_{block_n}")
+            optimize_vars[f"w_{block_n}"] =opt.maximize(w)
+            opt.add(w > 0)
+            opt.add(w < 10)
+            z3_variables[f"w_{block_n}"] = w
+        else:
+            var_left = Int(f"{ins[1]}_{block_n}_{block_variables_index[ins[1]]}")
+
+            z3_variables[f"{ins[1]}_{block_n}_{block_variables_index[ins[1]]}"] = var_left
+            block_variables_index[ins[1]] += 1
+            # first introduction of variable x and y
+            if ins[0] == Instr.mul and ins[2] == 0:
+                opt.add(var_left == 0)
+            else:
+                if ins[1] == 'z' and block_variables_index[ins[1]] - 2 < 0:
+                    var_right = z_in
+                else:
+                    var_right = z3_variables[f"{ins[1]}_{block_n}_{block_variables_index[ins[1]] - 2}"]
+                if isinstance(ins[2], int):
+                    if ins[0] == Instr.div:
+                        opt.add(var_left == var_right / ins[2])
+                    elif ins[0] == Instr.mod:
+                        opt.add(var_left == var_right % ins[2])
+                    elif ins[0] == Instr.add:
+                        opt.add(var_left == var_right + ins[2])
+                    elif ins[0] == Instr.mul:
+                        opt.add(var_left == var_right * ins[2])
+                    elif ins[0] == Instr.eql:
+                        opt.add(var_left == zeql(var_right, ins[2]))
+                    else:
+                        print("error")
+                elif ins[2] == 'z':
+
+                    opt.add(var_left == z_in)
+                else:
+                    if ins[2] == 'w':
+                        var_right2 = z3_variables[f"w_{block_n}"]
+                    else:
+                        var_right2 = z3_variables[f"{ins[2]}_{block_n}_{block_variables_index[ins[2]] - 1}"]
+                    if ins[0] == Instr.add:
+                        opt.add(var_left == var_right + var_right2)
+                    elif ins[0] == Instr.mul:
+                        opt.add(var_left == var_right * var_right2)
+                    elif ins[0] == Instr.eql:
+                        opt.add(var_left == zeql(var_right, var_right2))
+                    else:
+                        print("error")
+            if ins[1] == 'z':
+                z_last = var_left
+    return z_last
 
 
 def run_program(instructions, p_input, variables):
@@ -159,14 +195,14 @@ def run_program(instructions, p_input, variables):
 
 
 def part_2(lines):
-    pass
+    run_program()
 
 
 def main():
-    lines = get_lines("input_24.txt")
+    lines = get_lines("input_24.txt") # too high 91212119929999
     blocks = parse_input(lines)
     print("Part 1:", part_1(blocks))
-    print("Part 2:", part_2(lines))
+#    print("Part 2:", part_2(lines))
 
 
 if __name__ == '__main__':
