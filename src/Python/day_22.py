100bed67aa3f4f5eeb500ce31766611f607edb86
weichslgartner <weichslgartner@gmail.com>
2021-12-28 15:46:40 +0100

    day22 b

--- src/Python/day_22.py
+++ src/Python/day_22.py
@@ -1,5 +1,4 @@
 from functools import reduce
-from itertools import product
 
 from aoc import get_lines, Point3
 import re
@@ -18,28 +17,11 @@ def calc_volume(points):
 
 def parse_input(lines):
     return [True if line.startswith("on") else False for line in lines], \
-           [list(map(int, re.findall(r"-?\d+", line))) for line in lines]
-
-
-class Cube:
-    def __init__(self, on=True, next_cube=None):
-        self.on = on
-        self.next = next_cube
+           [tuple(map(int, re.findall(r"-?\d+", line))) for line in lines]
 
 
 def part_1(on_off, cubes):
-    points = set()
-    for turn_on, cube in zip(on_off, cubes):
-        if abs(cube[0]) > 50:
-            continue
-        cube_points = to_points(cube)
-        if turn_on:
-            points |= cube_points
-        else:
-           # print(points - cube_points)
-            points -= cube_points
-        print(len(points))
-    return len(points)
+    return solve(on_off, cubes, True)
 
 
 def to_points(cube):
@@ -52,55 +34,42 @@ def to_points(cube):
 
 
 def part_2(on_off, cubes):
-    points_debug_1 = set()
-    points_debug_2 = set()
+    return solve(on_off, cubes, False)
+
+
+def solve(on_off, cubes, max50: bool) -> int:
     cube_archive = set()
     n_turned = 0
     for turn_on, cube in zip(on_off, cubes):
+        if max50 and abs(cube[0]) > 50:
+            continue
         vol = calc_volume(cube)
         overlapped = []
+        overlapped_debug = {}
         to_add = set()
         to_remove = set()
-        new_on = []
         for other in cube_archive:
             overlap = get_overlap(cube, other)
             if overlap is not None:
                 new_cubes = split_cubes(other, overlap)
                 for c in new_cubes:
-                    to_add.add(tuple(c))
+                    to_add.add(c)
                 overlapped.append(overlap)
-                to_remove.add(tuple(other))
-
+                to_remove.add(other)
         if turn_on:
-           # points_debug_2 = to_points(cube)
-
-            cube_archive.add(tuple(cube))
-            if len(overlapped) >0:
+            cube_archive.add(cube)
+            if len(overlapped) > 0:
                 for o in overlapped:
                     # check if any new cubes overlap
                     vol -= calc_volume(o)
-                    #points_debug_2 -= to_points(o)
             n_turned += vol
-          #  if (to_points(cube) - points_debug_1) != (points_debug_2):
-          #      print("debug")
-          #  assert (to_points(cube) - points_debug_1) == (points_debug_2)
 
-            points_debug_1 |= to_points(cube)
         else:
-           # points_debug_1 = set()
-          #  points_debug_2 = set()
             if len(overlapped) > 0:
                 for o in overlapped:
-                  #  print(to_points(o))
                     n_turned -= calc_volume(o)
-                  #  points_debug_2 += to_points(o)
-           # print(to_points(cube) & points_debug_1)
-          #  print((points_debug_1 - to_points(cube)).intersection(points_debug_1))
-         #   assert ( (points_debug_1 - to_points(cube)).intersection(points_debug_1)   ) == points_debug_2
-          #  points_debug_1 -= to_points(cube)
         cube_archive |= to_add
         cube_archive -= to_remove
-        print(n_turned)
     return n_turned
 
 
@@ -114,11 +83,15 @@ def split_cubes(cube, sub_cube):
         x_section = x_section[2:]
     if x_section[-2] > x_section[-1]:
         x_section = x_section[:-2]
+    assert cube[Y_MIN] <= sub_cube[Y_MIN] <= cube[Y_MAX]
+    assert cube[Y_MIN] <= sub_cube[Y_MAX] <= cube[Y_MAX]
     y_section = [cube[Y_MIN], sub_cube[Y_MIN] - 1, sub_cube[Y_MIN], sub_cube[Y_MAX], sub_cube[Y_MAX] + 1, cube[Y_MAX]]
     if y_section[1] < y_section[0]:
         y_section = y_section[2:]
     if y_section[-2] > y_section[-1]:
         y_section = y_section[:-2]
+    assert cube[Z_MIN] <= sub_cube[Z_MIN] <= cube[Z_MAX]
+    assert cube[Z_MIN] <= sub_cube[Z_MAX] <= cube[Z_MAX]
     z_section = [cube[Z_MIN], sub_cube[Z_MIN] - 1, sub_cube[Z_MIN], sub_cube[Z_MAX], sub_cube[Z_MAX] + 1, cube[Z_MAX]]
     if z_section[1] < z_section[0]:
         z_section = z_section[2:]
@@ -132,7 +105,7 @@ def split_cubes(cube, sub_cube):
                 if not (x1 == sub_cube[X_MIN] and x2 == sub_cube[X_MAX] and
                         y1 == sub_cube[Y_MIN] and y2 == sub_cube[Y_MAX] and
                         z1 == sub_cube[Z_MIN] and z2 == sub_cube[Z_MAX]):
-                    cubes.append([x1, x2, y1, y2, z1, z2])
+                    cubes.append((x1, x2, y1, y2, z1, z2))
     return cubes
 
 
@@ -141,18 +114,12 @@ def get_overlap(cube1, cube2):
     y_overlap = None
     z_overlap = None
     # x overlap 1
-    if cube1[X_MIN] <= cube2[X_MIN] <= cube1[X_MAX]:
-        x_overlap = [cube2[X_MIN], min(cube1[X_MAX], cube2[X_MAX])]
-    if cube1[X_MIN] <= cube2[X_MAX] <= cube1[X_MAX]:
-        x_overlap = [max(cube1[X_MIN], cube2[X_MIN]), cube2[X_MAX]]
-    if cube1[Y_MIN] <= cube2[Y_MIN] <= cube1[Y_MAX]:
-        y_overlap = [cube2[Y_MIN], min(cube1[Y_MAX], cube2[Y_MAX])]
-    if cube1[Y_MIN] <= cube2[Y_MAX] <= cube1[Y_MAX]:
-        y_overlap = [max(cube1[Y_MIN], cube2[Y_MIN]), cube2[Y_MAX]]
-    if cube1[Z_MIN] <= cube2[Z_MIN] <= cube1[Z_MAX]:
-        z_overlap = [cube2[Z_MIN], min(cube1[Z_MAX], cube2[Z_MAX])]
-    if cube1[Z_MIN] <= cube2[Z_MAX] <= cube1[Z_MAX]:
-        z_overlap = [max(cube1[Z_MIN], cube2[Z_MIN]), cube2[Z_MAX]]
+    if max(cube1[X_MIN], cube2[X_MIN]) <= min(cube1[X_MAX], cube2[X_MAX]):
+        x_overlap = [max(cube1[X_MIN], cube2[X_MIN]), min(cube1[X_MAX], cube2[X_MAX])]
+    if max(cube1[Y_MIN], cube2[Y_MIN]) <= min(cube1[Y_MAX], cube2[Y_MAX]):
+        y_overlap = [max(cube1[Y_MIN], cube2[Y_MIN]), min(cube1[Y_MAX], cube2[Y_MAX])]
+    if max(cube1[Z_MIN], cube2[Z_MIN]) <= min(cube1[Z_MAX], cube2[Z_MAX]):
+        z_overlap = [max(cube1[Z_MIN], cube2[Z_MIN]), min(cube1[Z_MAX], cube2[Z_MAX])]
     if x_overlap is not None and y_overlap is not None and z_overlap is not None:
         return x_overlap + y_overlap + z_overlap
     return None
@@ -169,11 +136,26 @@ def tests():
     assert get_overlap([10, 11, 10, 11, 10, 11], [10, 12, 10, 12, 10, 12]) == [10, 11, 10, 11, 10, 11]
     assert len(split_cubes([10, 12, 10, 12, 10, 12], sub_cube=[10, 11, 10, 11, 10, 11])) == 7
     assert len(split_cubes([10, 13, 10, 13, 10, 13], sub_cube=[11, 11, 11, 11, 11, 11])) == 26
+    assert set(to_points([10, 12, 10, 12, 10, 12])).difference({Point3(11, 11, 11)}) == cubes_to_set(
+        split_cubes([10, 12, 10, 12, 10, 12], sub_cube=[11, 11, 11, 11, 11, 11]))
+    assert set(to_points([10, 12, 10, 12, 10, 12])).difference(to_points([10, 11, 10, 11, 10, 11])) == cubes_to_set(
+        split_cubes([10, 12, 10, 12, 10, 12], sub_cube=[10, 11, 10, 11, 10, 11]))
+    overlap = get_overlap([-20, 33, -21, 23, -26, 28], [-20, 26, -36, 17, -47, 7])
+    assert set(to_points([-20, 26, -36, 17, -47, 7])) - to_points(overlap) == cubes_to_set(
+        split_cubes([-20, 26, -36, 17, -47, 7], sub_cube=overlap))
+    get_overlap([-48, -32, 26, 41, -47, -37], [-46, -23, 21, 46, -50, -30]) == [-46, -23, 21, 46, -47, -37]
+
+
+def cubes_to_set(cubes):
+    points = set()
+    for c in cubes:
+        points |= to_points(c)
+    return points
 
 
 def main():
     tests()
-    lines = get_lines("input_22_test1.txt")
+    lines = get_lines("input_22.txt")
     on_off, cubes = parse_input(lines)
     print("Part 1:", part_1(on_off, cubes))
     print("Part 2:", part_2(on_off, cubes))
